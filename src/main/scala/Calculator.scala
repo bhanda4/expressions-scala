db1229d7648cd0a99fec5cb39a778daafb55979d
bhanda4 <112720453+bhanda4@users.noreply.github.com>
2022-11-09 21:36:59 -0600

    Initial commit

---
+++ src/main/scala/Calculator.scala
@@ -0,0 +1,32 @@
+package edu.luc.cs.laufer.cs371.expressions
+
+object Calculator:
+
+  def processExpr(input: String): Unit =
+    println("You entered: " + input)
+    val result = ASTBuilder.parseAll(ASTBuilder.expr, input)
+    if result.isEmpty then
+      println("This expression could not be parsed")
+    else
+      import org.json4s.native.JsonMethods.{pretty, render}
+      import behaviors.*
+      val raw = RawBuilder.parseAll(RawBuilder.expr, input).get
+      println("The untyped parse tree is: " + raw)
+      val expr = result.get
+      println("The resulting expression is: " + expr)
+      println("The corresponding JSON structure is:")
+      println(pretty(render(toJson(expr))))
+      println("It has size " + size(expr) + " and height " + height(expr))
+      println("It evaluates to " + evaluate(expr))
+
+  def main(args: Array[String]): Unit =
+    if args.length > 0 then
+      processExpr(args mkString " ")
+    else
+      print("Enter infix expression: ")
+      scala.io.Source.stdin.getLines() foreach { line =>
+        processExpr(line)
+        print("Enter infix expression: ")
+      }
+
+end Calculator
